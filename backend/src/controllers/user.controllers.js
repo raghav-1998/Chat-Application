e3d4830f2e25d0cef71880522b4f6d74cf1730ed
raghav-1998 <raghav.vasdeva@gmail.com>
2024-04-20 13:42:06 +0530

    Developing Register User Controller

---
+++ backend/src/controllers/user.controllers.js
@@ -0,0 +1,50 @@
+import { User } from "../models/user.model"
+
+const registerUser=async(req,res,next)=>{
+    try {
+        const {fullName,userName,email,password}=req.body
+        if([fullName,userName,email,password].some((field)=>field?.trim()==='')){
+            throw new Error("All field are required")
+        }
+
+        const existedUser=await User.findOne({
+            $or:[{userName},{email}]
+        })
+
+        if(existedUser){
+            throw new Error("User Already exist")
+        }
+
+        const user=await User.create({
+            fullName,
+            userName,
+            email,
+            password
+        })
+
+        const createdUser=await User.findById(user._id).select("-password -refreshToken")
+
+        if(!createdUser){
+            throw new Error("Something went wrong while registering User")
+        }
+
+        return res
+        .status(201)
+        .json({
+            statusCode:201,
+            data:createdUser,
+            message:"User Created Successfully"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+/*
+const loginUser=async(req,res,next)=>{
+    
+}*/
+
+export {
+    registerUser
+}
