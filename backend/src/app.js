cdca340ababc0becee593e2329381ed34861b5af
raghav-1998 <raghav.vasdeva@gmail.com>
2024-04-21 14:43:41 +0530

    Connecting Database with application

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,4 +5,8 @@ const app=express()
 app.use(express.json({limit:"20kb"}))
 app.use(express.urlencoded({extended:true,limit:"20kb"}))
 
+import userRoutes from './routes/user.routes.js'
+
+app.use('/user',userRoutes)
+
 export default app;
