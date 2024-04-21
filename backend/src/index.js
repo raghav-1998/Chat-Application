4b11a16f02867fbf72632b197d0190e997d4c37b
raghav-1998 <raghav.vasdeva@gmail.com>
2024-04-21 14:47:28 +0530

    Connecting Database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,25 @@
 import dotenv from'dotenv'
 import app from './app.js'
+import connectDB from './db/index.js'
 dotenv.config({
     path:'./.env'
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log('App is listing on port',process.env.PORT)
+connectDB()
+.then(
+    ()=>{
+        //App is not connecting to db even after db get connected successfully
+        app.on("error",(error)=>{
+            console.log('Error:',error)
+            process.exit(1)
+        })
+        
+        // App is listening the db
+        app.listen(process.env.PORT,()=>{
+            console.log('App is listening on port',process.env.PORT)
+        })
+    }
+)
+.catch((error)=>{
+    console.log('Mongo DB connection failed',error)
 })
